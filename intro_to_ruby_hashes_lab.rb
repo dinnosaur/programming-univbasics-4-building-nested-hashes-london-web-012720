832b5d60d60205004cf16f07e15f1e4abed5c5e9
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-22 19:52:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
     :rent_in_dollars => {
     :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
      },
-     :names => {:reading_railroad => "nursery", :road2 => "abercorn",:road3 => "welbeck", :road4 => "random" },
+     :names => {:reading_railroad => {}}, :pennsylvania_railroad => {}},:road3 => "welbeck", :road4 => "random" },
      
       }}
 end
